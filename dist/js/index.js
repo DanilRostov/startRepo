121acc6efecffb938dce3308bdf4829dc948aa7a
DanilRostov <daniil.rossy@gmail.com>
2018-01-30 22:45:56 +0300

    hello world

---
+++ dist/js/index.js
@@ -0,0 +1,2 @@
+console.log("Hello World");
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZpcnN0LWZpbGUuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUFBLFFBQUFDLElBQUEiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8gV29ybGQnKTsiXX0=
